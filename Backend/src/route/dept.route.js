cb5b539625d93aef453ad73125c1fabf7ca90c41
adimore96 <91413755+adimore96@users.noreply.github.com>
2025-08-11 23:32:55 +0530

    created the addProduct route

--- Backend/src/route/dept.route.js
+++ Backend/src/route/dept.route.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 const route = express.Router();
+const prodController = require("../contorller/dept.controller");
 
 route.get("/",(req,res)=>{
     res.send("<center><h1>Product Route</h1></center>");
-})
+});
+
+route.post("/addProduct",prodController.addDepartment);
+
 
 
 module.exports = route;
